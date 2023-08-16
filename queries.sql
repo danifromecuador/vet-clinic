1b3c5b25208d5fcfe6e0be61e6afd0c56195488f
dani <danifromecuador@gmail.com>
2023-08-16 01:16:33 -0500

    Remove double blank lines

--- queries.sql
+++ queries.sql
@@ -22,7 +22,6 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-
 -- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
 BEGIN;
 UPDATE animals SET species = 'unspecified';
@@ -78,5 +77,3 @@ SELECT species, AVG(escape_attempts) AS avg_escape_attempts
 FROM animals
 WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000
 GROUP BY species;
-
-
