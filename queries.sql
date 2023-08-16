a59efb9cc94f13f6774cd533690f184068576af7
dani <danifromecuador@gmail.com>
2023-08-16 01:08:11 -0500

    Delete ans Create a savepoint inside a trasaction

--- queries.sql
+++ queries.sql
@@ -41,5 +41,21 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 
+/*
+- Inside a transaction:
+  - Delete all animals born after Jan 1st, 2022.
+  - Create a savepoint for the transaction.
+  - Update all animals' weight to be their weight multiplied by -1.
+  - Rollback to the savepoint
+  - Update all animals' weights that are negative to be their weight multiplied by -1.
+  - Commit transaction
+*/
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT savepoint_1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO savepoint_1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
 
 
