394cef3c6c630e1b09b9687d03b0c85df3976604
dani <danifromecuador@gmail.com>
2023-08-15 22:37:01 -0500

    Add a new column called species

--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ vet_clinic=# CREATE TABLE animals (
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL(10, 2)
+    weight_kg DECIMAL(10, 2),
+    species VARCHAR(255)
 );
