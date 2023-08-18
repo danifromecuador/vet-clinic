7b304bcb90c7784d4ffe14bf9a7eecd1b0154994
dani <danifromecuador@gmail.com>
2023-08-17 20:12:00 -0500

    Modify animals table with owner_id info

--- data.sql
+++ data.sql
@@ -30,3 +30,26 @@ VALUES ('Blossom', '1998-10-13', 3, TRUE, 17, NULL);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
 VALUES ('Ditto', '2022-05-14', 4, TRUE, 22, NULL);
+
+INSERT INTO owners
+    (full_name, age)
+    VALUES
+        ('Sam Smith', 34),
+        ('Jennifer Orwell', 19),
+        ('Bob', 45),
+        ('Melody Pond', 77),
+        ('Dean Winchester', 14),
+        ('Jodie Whittaker', 38);
+
+INSERT INTO species
+    (name)
+    VALUES
+        ('Pokemon'),
+        ('Digimon');
+
+UPDATE animals
+SET species_id = 
+    (CASE
+        WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+        ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+    END);
