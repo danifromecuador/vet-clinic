f6b28212e0cb838046932b29ee8c505c7d63651b
dani <danifromecuador@gmail.com>
2023-08-18 22:27:23 -0500

    Add relation data in specializations table

--- data.sql
+++ data.sql
@@ -69,3 +69,9 @@ INSERT INTO vets (name, age, date_of_graduation) VALUES
     ('Maisy Smith', 26, '2019-01-17'),
     ('Stephanie Mendez', 64, '1981-05-04'),
     ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vet_id, species_id) VALUES
+    ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM species WHERE name = 'Pokemon')),
+    ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Digimon')),
+    ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM species WHERE name = 'Pokemon')),
+    ((SELECT id FROM vets WHERE name = 'Jack Harkness'), (SELECT id FROM species WHERE name = 'Digimon'));
