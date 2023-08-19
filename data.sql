9d8613ddf3eb5ee0927cc227212e2cae1a0ad394
dani <danifromecuador@gmail.com>
2023-08-18 22:24:08 -0500

    Add data to vets table

--- data.sql
+++ data.sql
@@ -64,3 +64,8 @@ SET owner_id =
         WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
     END;
 
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+    ('William Tatcher', 45, '2000-04-23'),
+    ('Maisy Smith', 26, '2019-01-17'),
+    ('Stephanie Mendez', 64, '1981-05-04'),
+    ('Jack Harkness', 38, '2008-06-08');
